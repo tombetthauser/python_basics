097fcabf76221814d2a7f6bf9a6274bdd93f6b17
tombetthauser <tombetthauser@gmail.com>
2020-04-27 12:50:35 -0700

    Initial upload and lesson 10 complete

---
+++ basics.py
@@ -0,0 +1,32 @@
+### SECTION 03 - The Basic Datatypes
+
+
+## 10. Variables ---------------------------------------
+
+# print('hello world!')
+
+# my-bad-var = 'hello there!'
+# print(my-bad-var)
+
+# my_good_var = 'heya buddy!'
+# print(my_good_var)
+
+# myCamelVar = 'whassup bud?'
+# print(myCamelVar)
+
+# myBadVar02 = 'why does this work?'
+# print(myBadVar02)
+
+# 03badVar = `this is bad for so many raisins...`
+# print(03badVar)
+
+# import datetime
+# today = datetime.datetime.now()
+# print("today's datetime = ", today)
+
+# mynumber = 5
+# mytext = 'hello again!'
+# print(mynumber, mytext)
+
+
+## Coding Exercise 01 ---------------------------------------
