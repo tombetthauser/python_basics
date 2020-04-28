825714ea9e1a30f572641afb8d9fcd9ab29099b5
tombetthauser <tombetthauser@gmail.com>
2020-04-28 16:37:23 -0700

    Completes up through exercise 35

--- basics.py
+++ basics.py
@@ -390,8 +390,15 @@
 # Marry Simpons has as phone number + 423998200919
 
 
+# phone_numbers = {"John Smith": "+37682929928",
+#                  "Marry Simpons": "+423998200919"}
+
+# for key, val in phone_numbers.items():
+#     print("{}: {}".format(key, val))
+
+
 phone_numbers = {"John Smith": "+37682929928",
                  "Marry Simpons": "+423998200919"}
 
-for key, val in phone_numbers.items():
-    print("{}: {}".format(key, val))
+for val in phone_numbers.values():
+    print("00{}".format(val[1:]))
