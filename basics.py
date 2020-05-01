e88bd19de1faf55358d6e36f62b74d4afdebd572
tombetthauser <tombetthauser@gmail.com>
2020-04-30 22:49:11 -0700

    Thurs night update

--- basics.py
+++ basics.py
@@ -560,28 +560,29 @@
 # for ele in content:
 #   print(ele)
 
-Summary: File Processing
+# Summary: File Processing
 
-In this section you learned that:
+# In this section you learned that:
 
-    You can read an existing file with Python:
+#     You can read an existing file with Python:
 
-    with open("file.txt") as file:
-        content = file.read()
+#     with open("file.txt") as file:
+#         content = file.read()
 
-    You can create a new file with Python and write some text on it:
+#     You can create a new file with Python and write some text on it:
 
-    with open("file.txt", "w") as file:
-        content = file.write("Sample text")
+#     with open("file.txt", "w") as file:
+#         content = file.write("Sample text")
 
-    You can append text to an existing file without overwriting it:
+#     You can append text to an existing file without overwriting it:
 
-    with open("file.txt", "a") as file:
-        content = file.write("More sample text")
+#     with open("file.txt", "a") as file:
+#         content = file.write("More sample text")
 
-    You can both append and read a file with:
+#     You can both append and read a file with:
 
-    with open("file.txt", "a+") as file:
-        content = file.write("Even more sample text")
-        file.seek(0)
-        content = file.read()
+#     with open("file.txt", "a+") as file:
+#         content = file.write("Even more sample text")
+#         file.seek(0)
+#         content = file.read()
+
