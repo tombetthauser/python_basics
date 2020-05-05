af2d1ff7b03d802c2383fcbc54f5b3136321e68c
tombetthauser <tombetthauser@gmail.com>
2020-05-04 21:45:56 -0700

    Add notes on importing json into python with pandas

--- pandas_practice.py
+++ pandas_practice.py
@@ -18,13 +18,38 @@
 # once youre done with a session just close the browser tabs and to reopen just relocate to directory with a notebook and command line $ jupyter notebook
 
 
+
+# Solution
+
+# The code for loading the supermarkets.json file in Python with pandas would be this:
+
+#     import pandas
+#     df2 = pandas.read_json("supermarkets.json")
+
+# The df2 dataframe should contain this data:
+
+
+
+
+
+
 # Exercise: Loading JSON Files
 
-In  the previous lecture you learned that you can load a CSV file with this code:
+# In  the previous lecture you learned that you can load a CSV file with this code:
 
-    import pandas
-    df1 = pandas.read_csv("supermarkets.csv")
+#     import pandas
+#     df1 = pandas.read_csv("supermarkets.csv")
 
-Try loading the supermarkets.json file for this exercise using read_json instead of read_csv.
+# Try loading the supermarkets.json file for this exercise using read_json instead of read_csv.
 
-The supermarkets.json file can be found inside the supermarkets.zip file attached in the previous lecture.
+# The supermarkets.json file can be found inside the supermarkets.zip file attached in the previous lecture.
+
+
+Note on Loading Excel Files
+
+In the next lecture you're also going to learn how to load Excel (.xlsx) files in Python with pandas. Pandas may require the xlrd library as a dependency. If you get an error such as ModuleNotFoundError: No module named 'xlrd', you can fix the error by installing xlrd:
+
+pip install xlrd
+or
+
+pip3 install xlrd
