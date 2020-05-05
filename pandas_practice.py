1acc815d3e364e9e0160fd2075b1e4589b361b8d
tombetthauser <tombetthauser@gmail.com>
2020-05-04 18:37:16 -0700

    Clean up pandas notes from bad echo appends

--- pandas_practice.py
+++ pandas_practice.py
@@ -1,7 +1,11 @@
 #remember you have to 'pip3 install pandas' and pip3 install ipython before working with these
-\n#
-\n# ipython is just an enhanced interface for working with data in python
-\n#
-\n# pandas is a library that allows us to easily work with a lot of different database formats like json etc
-\n#
-\n# when you enter into ipython you have to manually import pandas
+#
+# ipython is just an enhanced interface for working with data in python
+#
+# pandas is a library that allows us to easily work with a lot of different database formats like json etc
+#
+# when you enter into ipython you have to manually import pandas
+
+
+
+# test
